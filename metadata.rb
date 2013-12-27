7c899ab21413fbe2036549701c65dcc52ce0fa9d
elliotkendallUCSF <elliot.kendall@ucsf.edu>
2013-12-27 11:16:20 -0800

    Switch to ruby metadata

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+maintainer        "Elliot Kendall"
+maintainer_email  "elliot.kendall@ucsf.edu"
+license           "Apache 2.0"
+description       "Installs OpenDJ LDAP server"
+version           "0.1.1"
+
+recipe "opendj", "Installs OpenDJ LDAP server"
+
+%w{ redhat centos fedora ubuntu debian }.each do |os|
+  supports os
+end
